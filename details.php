526504debdf0370cff97e50d3aab1c0b5121e78f
fotrise2 <fotrise2@gmail.com>
2018-11-27 17:17:08 +0900

    details page 작업

--- details.php
+++ details.php
@@ -1,39 +1,56 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="uft-8"/>
-    <title>GoBong</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="theme-color" content="#6B58A7">
-    <!--<link href="/assets/images/common/favi.ico" rel="shortcut icon">
-    <meta name="naver-site-verification" content="5a3760839632bf2accaa65c28ee953451801fbbc"/>
-    <meta name="description" content="콘서트예매, MD상품을 손쉽게 구매하는 종합몰">
-    <meta name="keywords" content="KSTAR, 케이스타, 블록체인, 암호화폐 결제">
-    <meta name="author" content="KSTAR">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="KSTAR">
-    <meta property="og:description" content="KSTAR">
-    <meta property="og:url" content="https://www.kstar.tv">
-    <link rel="canonical" href="https://www.kstar.tv">-->
-    <link rel="stylesheet" href="resources/style/index.css"/>
-    <script type="text/javascript" src="resources/js/vendor/jquery-2.2.4.min.js"></script>
-    <script type="text/javascript" src="resources/js/pages/index.js"></script>
+  <meta charset="uft-8"/>
+  <title>GoBong</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="theme-color" content="#6B58A7">
+  <!--<link href="/assets/images/common/favi.ico" rel="shortcut icon">
+  <meta name="naver-site-verification" content="5a3760839632bf2accaa65c28ee953451801fbbc"/>
+  <meta name="description" content="콘서트예매, MD상품을 손쉽게 구매하는 종합몰">
+  <meta name="keywords" content="KSTAR, 케이스타, 블록체인, 암호화폐 결제">
+  <meta name="author" content="KSTAR">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="KSTAR">
+  <meta property="og:description" content="KSTAR">
+  <meta property="og:url" content="https://www.kstar.tv">
+  <link rel="canonical" href="https://www.kstar.tv">-->
+  <link rel="stylesheet" href="resources/style/index.css"/>
+  <script type="text/javascript" src="resources/js/vendor/jquery-2.2.4.min.js"></script>
+  <script type="text/javascript" src="resources/js/pages/index.js"></script>
 </head>
 <body>
 <section class="details__wrap">
-    <header class="details__hd">
-        <a href="index.html">Back To Home</a>
-    </header>
+  <!-- HEADER -->
+  <header class="details__hd">
+    <a href="index.html">Back To Home</a>
+  </header>
 
-    <section class="layout details-content">
+  <section class="layout details-content">
+    <h1 class="title"></h1>
+    <div class="title-line"></div>
+    <p class="details__txt"></p>
 
-    </section>
-    <!-- FOOTER -->
-    <footer class="footer">
-        <p>2018 Bong-Hoon Ko. all rights reserved.</p>
-    </footer>
+    <img class="details__img notebook" src="" alt=""/>
+    <img class="details__img full mobile" src="" alt=""/>
+  </section>
+
+  <!-- FOOTER -->
+  <footer class="footer">
+    <p>2018 Bong-Hoon Ko. all rights reserved.</p>
+  </footer>
 </section>
+<?php
+echo "<script>var page = '".$_GET['page']."';</script>";
+?>
+<script>
+  $('.details-content .title').html(work_lists[page].conTitle);
+  $('.details-content .sub-title').html(work_lists[page].conSubTitle);
+  $('.details-content .details__txt').html(work_lists[page].conTxt);
+  $('.details__img.notebook').attr('src','resources/images/'+work_lists[page].details.noteBook);
+  $('.details__img.mobile').attr('src','resources/images/'+work_lists[page].details.mobile);
+</script>
 </body>
 </html>
--- resources/js/pages/index.js
+++ resources/js/pages/index.js
@@ -20,51 +20,63 @@ $(function () {
 
 var work_lists = {
     "work1": {
-        "conTitle": "RAON",
-        "conTxt": "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.0",
-        "conImg": "raon_thumb.jpg",
-        "conAlt": "raon thumbnail"
+        "conTitle"    : "RAON",
+        "conSubTitle" : "",
+        "conTxt"      : "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.0",
+        "conImg"      : "raon_thumb.jpg",
+        "conAlt"      : "raon thumbnail",
+        "details"     : {
+            "noteBook"  : "kstar2.5_nb.png",
+            "mobile"    : "kstar2.5Mobile_2.jpg"
+        }
     },
     "work2": {
-        "conTitle": "OPUS DESIGN",
-        "conTxt": "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.1",
-        "conImg": "opus_thumb.jpg",
-        "conAlt": "opus-design thumbnail"
+        "conTitle"    : "OPUS DESIGN",
+        "conSubTitle" : "",
+        "conTxt"      : "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.1",
+        "conImg"      : "opus_thumb.jpg",
+        "conAlt"      : "opus-design thumbnail"
     },
     "work3": {
-        "conTitle": "KSTAR 2.0",
-        "conTxt": "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.2",
-        "conImg": "kstar2.0.jpg",
-        "conAlt": "kstar2.0 thumbnail"
+        "conTitle"    : "KSTAR <span class='inherit__txt'>2.0</span>",
+        "conSubTitle" : "",
+        "conTxt"      : "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.2",
+        "conImg"      : "kstar2.0.jpg",
+        "conAlt"      : "kstar2.0 thumbnail"
     },
     "work4": {
-        "conTitle": "KSTAR 2.5",
-        "conTxt": "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.3",
-        "conImg": "kstar2.51.jpg",
-        "conAlt": "kstar2.5 thumbnail"
+        "conTitle"    : "KSTAR <span class='inherit__txt'>2.5</span>",
+        "conSubTitle" : "",
+        "conTxt"      : "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.3",
+        "conImg"      : "kstar2.51.jpg",
+        "conAlt"      : "kstar2.5 thumbnail"
     },
     "work5": {
-        "conTitle": "STARPAY Main",
-        "conTxt": "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.4",
-        "conImg": "pay.jpg",
-        "conAlt": "starpay thumbnail"
+        "conTitle"    : "STARPAY Main",
+        "conSubTitle" : "",
+        "conTxt"      : "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.4",
+        "conImg"      : "pay.jpg",
+        "conAlt"      : "starpay thumbnail"
     },
     "work6": {
-        "conTitle": "STARCOIN Main",
-        "conTxt": "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.5",
-        "conImg": "starcoin.jpg",
-        "conAlt": "starcoin thumbnail"
+        "conTitle"    : "STARCOIN Main",
+        "conSubTitle" : "",
+        "conTxt"      : "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.5",
+        "conImg"      : "starcoin.jpg",
+        "conAlt"      : "starcoin thumbnail"
     },
     "work7": {
-        "conTitle": "KSTAR SponsorDay",
-        "conTxt": "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.6",
-        "conImg": "sponday.jpg",
-        "conAlt": "kstar sponsor day thumbnail"
+        "conTitle"    : "KSTAR SponsorDay",
+        "conSubTitle" : "",
+        "conTxt"      : "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.6",
+        "conImg"      : "sponday.jpg",
+        "conAlt"      : "kstar sponsor day thumbnail"
     },
     "work8": {
-        "conTitle": "Condeal Intro",
-        "conTxt": "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.7",
-        "conImg": "condeal_intro.jpg",
-        "conAlt": "condeal intro thumbnail"
+        "conTitle"    : "Condeal Intro",
+        "conSubTitle" : "",
+        "conTxt"      : "다 계절이 불러 사람들의 그러나 이국 언덕 봅니다. 위에 지나고 아름다운 경, 내린 거외다.7",
+        "conImg"      : "condeal_intro.jpg",
+        "conAlt"      : "condeal intro thumbnail"
     }
 }
